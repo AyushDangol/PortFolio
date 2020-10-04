88a6967a09c94f7d0d3137d018daeca75603a4f2
ayushdangol215 <ayushdangol215@gmail.com>
2020-10-04 17:31:01 +0545

    Updates

---
+++ precache-manifest.28c67be55ee3818a741eddb70977e5bf.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b88772c66e72cfef17afabaed3ecfb",
+    "url": "/PortFolio/index.html"
+  },
+  {
+    "revision": "69aadedb69ef856f4705",
+    "url": "/PortFolio/static/css/2.5bdb7e52.chunk.css"
+  },
+  {
+    "revision": "4e19785786a9bb685315",
+    "url": "/PortFolio/static/css/main.927ec92b.chunk.css"
+  },
+  {
+    "revision": "69aadedb69ef856f4705",
+    "url": "/PortFolio/static/js/2.8b7495d6.chunk.js"
+  },
+  {
+    "revision": "eacb97db2345938743cf5a253fcf37d1",
+    "url": "/PortFolio/static/js/2.8b7495d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e19785786a9bb685315",
+    "url": "/PortFolio/static/js/main.9773e803.chunk.js"
+  },
+  {
+    "revision": "c7bbb472ec8175fc02a1",
+    "url": "/PortFolio/static/js/runtime-main.7f9fc357.js"
+  },
+  {
+    "revision": "83330aec12fdd5548b5ccd392c3ea9bc",
+    "url": "/PortFolio/static/media/FigmaTool.83330aec.svg"
+  },
+  {
+    "revision": "daae073b76d8914866edf3126114dc4c",
+    "url": "/PortFolio/static/media/Gitbash.daae073b.svg"
+  },
+  {
+    "revision": "95c8748dc4304774cee1d5127e2f6f99",
+    "url": "/PortFolio/static/media/JS.95c8748d.svg"
+  },
+  {
+    "revision": "e287a444430cd0807a2d01d33e4c7358",
+    "url": "/PortFolio/static/media/TeamPlayer.e287a444.svg"
+  },
+  {
+    "revision": "8e4c03e2c790339dd299314e762d8f90",
+    "url": "/PortFolio/static/media/Travello-Thumbnail.8e4c03e2.png"
+  },
+  {
+    "revision": "4a46cb94eb2bd345ddbf414b040f33e0",
+    "url": "/PortFolio/static/media/Vuejs.4a46cb94.svg"
+  },
+  {
+    "revision": "e556718a297245a8936965599066a8b1",
+    "url": "/PortFolio/static/media/facebook-dark.e556718a.svg"
+  },
+  {
+    "revision": "0c005f2ce6289c13377afc5dfb7edbdf",
+    "url": "/PortFolio/static/media/facebook.0c005f2c.svg"
+  },
+  {
+    "revision": "6806ee9a9779f40cb924bd347084bc29",
+    "url": "/PortFolio/static/media/icomoon.6806ee9a.svg"
+  },
+  {
+    "revision": "b82f20a708c5324385715e87df924c5e",
+    "url": "/PortFolio/static/media/icomoon.b82f20a7.eot"
+  },
+  {
+    "revision": "d207c63210c56898cb8caae34f285833",
+    "url": "/PortFolio/static/media/icomoon.d207c632.woff"
+  },
+  {
+    "revision": "ea706bfd16acbc6d42cd7904017f58c7",
+    "url": "/PortFolio/static/media/icomoon.ea706bfd.ttf"
+  },
+  {
+    "revision": "9ae7e3a79927fec6f5f13216b6960c3b",
+    "url": "/PortFolio/static/media/illustration2.9ae7e3a7.svg"
+  },
+  {
+    "revision": "104b0fec54a5318e4fedd855d568da14",
+    "url": "/PortFolio/static/media/instagram-dark.104b0fec.svg"
+  },
+  {
+    "revision": "2dfb26610d208f96c3dfc434c97e731a",
+    "url": "/PortFolio/static/media/instagram.2dfb2661.svg"
+  },
+  {
+    "revision": "e0c1662501379f67fc18cf8f83cf5401",
+    "url": "/PortFolio/static/media/linkedin-dark.e0c16625.svg"
+  },
+  {
+    "revision": "8068d00d5db92b566474f79e44b6448e",
+    "url": "/PortFolio/static/media/linkedin.8068d00d.svg"
+  },
+  {
+    "revision": "094daedcd9d473f91409e82e7d045f6c",
+    "url": "/PortFolio/static/media/profile.094daedc.pdf"
+  },
+  {
+    "revision": "86e2fc64412c30e1d13e0e6cd5b94193",
+    "url": "/PortFolio/static/media/reactjs.86e2fc64.svg"
+  },
+  {
+    "revision": "da084de196b2f28b03cc8bcc6f631e58",
+    "url": "/PortFolio/static/media/sassy.da084de1.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/PortFolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/PortFolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/PortFolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/PortFolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7dd78e643ae4343a566cc694a9c41adc",
+    "url": "/PortFolio/static/media/twitter-dark.7dd78e64.svg"
+  },
+  {
+    "revision": "59b92b56da088363a85d2b0c220ed455",
+    "url": "/PortFolio/static/media/twitter.59b92b56.svg"
+  }
+]);
